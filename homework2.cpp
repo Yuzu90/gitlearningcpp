ac95b6a5593fa51ca4c943edf6aec7ac68f693d5
Svenni Dal <svennidal@svennidal.com>
2016-08-16 17:19:28 +0000

    print out 2 variables with the same name, but diffirent namespace

--- homework2.cpp
+++ homework2.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
+#include <string>
 using namespace std;
 ////It's just a second homework which was supposed to learn me about using namespaces.
 ////I was supposed to make code lower there to work. I could do it in two ways.
 ////P.S. I think the homeworks for now are pretty easy but I am afraid in few more steps I'll get rekt by homework.
 
+namespace svenni {
+	string message = "Svenni::Znajdx dwa rozne rozwiazania tego zadania.";
+}
+namespace matti {
+	string message = "Matti::Znajdx dwa rozne rozwiazania tego zadania.";
+}
 
 int main()
 {
-	cout << "Znajdx dwa rozne rozwiazania tego zadania." << endl;
+	cout << svenni::message << endl;
+	cout << matti::message << endl;
 	return 0;
 }
