fd7e6f35794cdaef7d431daf72a7625eee972ed2
Yuzu90 <dsmg.poradnik@gmail.com>
2016-08-16 11:15:37 +0000

    adding homework1

---
+++ homework1.cpp
@@ -0,0 +1,27 @@
+#include <cstdio>
+#include <iostream>
+
+///////////////It's "homework" from my c++ course
+/*
+I've should to create something like that:
+==============
+= To Proste! =
+==============
+	A jak to zrobic?
+Koniec :)
+welp:
+*/
+
+
+
+
+
+int main()
+{
+	std::cout << "==============" << std::endl;
+	std::cout << "= To Proste! =" << std::endl;
+	std::cout << "==============" << std::endl;
+	printf( "\t A jak to zrobic? \n Koniec :)");
+	std::cout << std::endl;
+return 0;
+}
