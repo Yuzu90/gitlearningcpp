3f17498d45bfc0831aced0d705b78ec304f0a4db
Yuzu90 <dsmg.poradnik@gmail.com>
2016-08-16 12:26:39 +0000

    Adding Homework3.cpp

---
+++ homework3.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+int main()
+{
+int nieznana = 123;
+    std::cout << "Liczba = " << nieznana << std::endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*#include <iostream>
+int main()
+{
+    std::cout << "Liczba = " << nieznana << std::endl;
+    return 0;
+}
+
+
+My homework is to make this program works without deleting anything and it should print out number 123
+*/
