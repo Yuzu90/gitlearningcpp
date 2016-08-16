453db3acc782eaaf5ae1d4e83f26ae512cc72c50
Yuzu90 <dsmg.poradnik@gmail.com>
2016-08-16 11:23:09 +0000

    Adding Hello World.cpp

---
+++ helloworld.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main()
+{
+	/*
+	printf( "Hello World" );
+	*/ 
+	///It's the first way (using cstdio include) Use second one (using iostream include):
+	std::cout << "Hello World" << std::endl;
+	return 0;
+}
