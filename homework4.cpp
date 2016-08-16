2440f6a18344dfa8dff723a0cdfb6aeb8dc2fd42
Svenni Dal <svennidal@svennidal.com>
2016-08-16 18:17:10 +0000

    this is how I would solve for number 4

--- homework4.cpp
+++ homework4.cpp
@@ -2,44 +2,40 @@
 /// I need also to show if program could read the number and let the program to ignore yeti's.
 
 #include <iostream>
+#include <string>
 using namespace std;
 int main()
 {
-	int l1;
-	int l2;
-	int l3;
+	int l1, l2, l3;
+	bool b1, b2, b3;
 	
 	cout << "Enter first number:";
 	cin >> l1;
-	if (cin.fail()){
-		cout << "cin failed." << endl;
-		cin.clear();
-	}else{
-		cout << "cin succeeded." << endl;
-	}
+	b1 = cin.fail();
+	cin.clear();
+	cin.ignore(10000,'\n');
 	
 	
 	cout << "Enter second number:";
 	cin >> l2;
-		if (cin.fail()){
-			cout << "cin failed." << endl;
-			cin.clear();
-		}else{
-		cout << "cin succeeded." << endl;
-		}
+	b2 = cin.fail();
+	cin.clear();
+	cin.ignore(10000,'\n');
 			
 			
 	cout << "Enter third number:";
 	cin >> l3;
-	if (cin.fail()){
-		cout << "cin failed." << endl;
-		cin.clear();
-	}else{
-		cout << "cin succeeded." << endl;
-	}
+	b3 = cin.fail();
+	cin.clear();
+	cin.ignore(10000,'\n');
 		
-	cout << "First number was:" << l1 << endl << "Second number was :" << l2 << endl << "Third number was :" << l3 << endl;
-return 0;
+
+	cout << "First number was: " << (b1 ? "false" : to_string(l1)) << endl 
+		<< "Second number was: " << (b2 ? "false" : to_string(l2)) << endl 
+		<< "Third number was: " << (b3 ? "false" : to_string(l3)) << endl;
+
+
+	return 0;
 }
 
 ///I couldn't find how to save cin.fail() to some bool so I did it in "if". Help me.
