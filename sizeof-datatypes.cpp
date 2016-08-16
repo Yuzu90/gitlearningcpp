63a942299618e61796f2137cc7423c994901f7c0
Yuzu90 <dsmg.poradnik@gmail.com>
2016-08-16 12:22:12 +0000

    Adding sizeof-datatypes.cpp

---
+++ sizeof-datatypes.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdio>
+int main()
+{
+    std::cout << "sizeof(bool) = " << sizeof( bool ) << std::endl;
+    std::cout << "sizeof(char) = " << sizeof( char ) << std::endl;
+    std::cout << "sizeof(unsigned char) = " << sizeof( unsigned char ) << std::endl;
+    std::cout << "sizeof(wchar_t) = " << sizeof( wchar_t ) << std::endl;
+    std::cout << "sizeof(short) = " << sizeof( short ) << std::endl;
+    std::cout << "sizeof(unsigned short) = " << sizeof( unsigned short ) << std::endl;
+    std::cout << "sizeof(int) = " << sizeof( int ) << std::endl;
+    std::cout << "sizeof(unsigned int) = " << sizeof( unsigned int ) << std::endl;
+    std::cout << "sizeof(long) = " << sizeof( long ) << std::endl;
+    std::cout << "sizeof(unsigned long) = " << sizeof( unsigned long ) << std::endl;
+    std::cout << "sizeof(long long) = " << sizeof( long long ) << std::endl;
+    std::cout << "sizeof(float) = " << sizeof( float ) << std::endl;
+    std::cout << "sizeof(double) = " << sizeof( double ) << std::endl;
+    std::cout << "sizeof(long double) = " << sizeof( long double ) << std::endl;
+    return 0;
+}
