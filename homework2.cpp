99d4626fee49280beb17a6264de98793a1bb1a9d
Yuzu90 <dsmg.poradnik@gmail.com>
2016-08-16 12:19:28 +0000

    Adding homework2.cpp

---
+++ homework2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+////It's just a second homework which was supposed to learn me about using namespaces.
+////I was supposed to make code lower there to work. I could do it in two ways.
+////P.S. I think the homeworks for now are pretty easy but I am afraid in few more steps I'll get rekt by homework.
+
+
+int main()
+{
+	cout << "Znajdx dwa rozne rozwiazania tego zadania." << endl;
+	return 0;
+}
